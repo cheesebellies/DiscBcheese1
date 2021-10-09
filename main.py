8e85237299fe585c5ee348ac39f1e1b9118e4f5c
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2021-10-09 17:10:14 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+from discord.ext import tasks
+import discord
+import os
+from dotenv import load_dotenv
+import itertools
+
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+bot = commands.Bot(command_prefix=';')
+
+@bot.event
+async def on_ready():
+  for guild in bot.guilds:
+    print(
+    f'{bot.user} is connected to the following guild(s):\n'
+    f'{guild.name}(id: {guild.id})'
+    )
+  await change_status()
+
+
+status = itertools.cycle(['Music coming soon!','Status cycling!','online 24/7!'])
+
+@tasks.loop(seconds=20)
+async def change_status():
+  await bot.change_presence(activity=discord.Game(next(status)))
+
+bot.run(TOKEN)
